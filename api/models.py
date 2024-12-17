adaded9a90a26639f6b29e02ef15777ccbeb47a1
Tedpi1 <moroted16@gmail.com>
2024-12-17 04:53:49 +0300

    models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+
+class Note(models.Model):
+    title=models.charfield(max_length=100)
+    content=models.Textfield()
+    created_at=models.DateTimeField(auto_now_add=True)
+    author=models.ForeignKey(on_delete=Models.CASCADE, related_name="notes")
+    
+    
+    def __str__(self):
+        return self.title
 # Create your models here.
